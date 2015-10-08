7a40a0263c12aa4401d4c4a87fe906f240425270
margonaut <margaret.morgane@gmail.com>
2015-10-08 16:46:51 -0400

    More hound fixes

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -3,11 +3,11 @@ var replaceKeywords = function(keywords) {
   var list = $("#keyword-list");
   for (var i = 0; i < keywords.length; i++) {
     list.append("<li class=\"dream-key\">" + keywords[i].text + "</li>");
-  };
+  }
 }
 
 var changeDream = function(dream) {
-  var keywords = dream.dreams_keywords
+  var keywords = dream.dreams_keywords;
   var dreamTitle = $("#dream-title");
   var dreamText = $("#dream-text");
   dreamTitle.text(dream.title);
